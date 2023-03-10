f6ce4eae7fae472acf03e3a14243bc8d3810af8a
cchiu28 <scorpiusdx@gmail.com>
2023-03-10 11:56:58 -0500

    relocating firebaseconfig

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,16 @@ import App from './App'
 import './index.css'
 import { initializeApp } from 'firebase/app';
 
-// const firebaseConfig = {
-//   apiKey: `${import.meta.env.VITE_FIREBASE_KEY}`,
-//   authDomain: "meatup-28a8e.firebaseapp.com",
-//   projectId: "meatup-28a8e",
-//   storageBucket: "meatup-28a8e.appspot.com",
-//   messagingSenderId: "848230416037",
-//   appId: "1:848230416037:web:cf5661f0386f08e4cfd74c"
-// };
+const firebaseConfig = {
+  apiKey: `${import.meta.env.VITE_FIREBASE_KEY}`,
+  authDomain: "meatup-28a8e.firebaseapp.com",
+  projectId: "meatup-28a8e",
+  storageBucket: "meatup-28a8e.appspot.com",
+  messagingSenderId: "848230416037",
+  appId: "1:848230416037:web:cf5661f0386f08e4cfd74c"
+};
 
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
