5dbafd144d83a3484255e61d34f341a5aa52e5e8
cchiu28 <scorpiusdx@gmail.com>
2023-03-26 20:49:22 -0400

    added operating times to business page

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Rating from "./Rating";
+import { days, getTime, convert24To12 } from '../utils';
+import dayjs from "dayjs";
 
 interface Business {
     id: string,
@@ -35,46 +37,89 @@ interface Time {
     is_overnight: boolean,
 }
 
+interface biz {
+    business: Business,
+    reviews: Reviews[],
+}
+
+interface Reviews {
+    total: number,
+    reviews: []
+}
+
 export default function Business() {
     const loc = useLocation();
-    const [biz, setBiz] = useState<Business>();
+    const [biz, setBiz] = useState<biz>();
+    // const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
     useEffect(() => {
         (async () => {
             // const res = await fetch(`https://meatup-cmdt.onrender.com/api/business`, {
-                const res = await fetch('http://localhost:8080/api/business' , {
+            const business = await fetch('http://localhost:8080/api/business' , {
                 method: 'POST',
                 body: loc.state.id,
             });
-            const data = await res.json();
-            console.log(data)
-            setBiz(data);
+            const reviews = await fetch('http://localhost:8080/api/reviews', {
+                method: 'POST',
+                body: loc.state.id,
+            })
+            const businessData = await business.json();
+            const reviewData = await reviews.json();
+            console.log(businessData)
+            setBiz({
+                    business: businessData,
+                    reviews: reviewData,
+            });
         })();
     },[])
 
     return(
         <>
             {biz &&
-            <div>
-                <div className="relative flex flex-row w-full shadow-md border border-inherit rounded-md">
-                        {biz.photos.map((pic,index) => {
-                            return(
-                                <figure key={index} className="flex w-full brightness-50">
-                                    <img src={pic} className="object-cover"/>
-                                </figure>
-                            )
-                        })}
-                        <div className="absolute bottom-10 left-32 w-auto border border-red-500">
-                            <h1 className="text-5xl text-white font-extrabold">{biz.name}</h1>
-                            <div className="flex justify-start m-1 w-auto">
-                                <Rating rating={biz.rating}/>
-                                <span>{biz.review_count}</span>
+                <div>
+                    <div className="relative flex flex-row w-full shadow-md border border-inherit rounded-md">
+                            {biz.business.photos.map((pic,index) => {
+                                return(
+                                    <figure key={index} className="flex w-full brightness-50">
+                                        <img src={pic} className="object-cover"/>
+                                    </figure>
+                                )
+                            })}
+                            <div className="absolute bottom-10 left-32 w-auto border border-red-500">
+                                <h1 className="text-5xl text-white font-extrabold">{biz.business.name}</h1>
+                                <div className="flex justify-start m-1 w-auto">
+                                    <Rating rating={biz.business.rating}/>
+                                    <span>{biz.business.review_count}</span>
+                                </div>
                             </div>
-                        </div>
+                    </div>
+                    <div className="container mx-auto flex flex-col">
+                        <table className="border border-inherit">
+                            <thead>
+                                <tr className="bg-slate-300">
+                                    <th className="border border-inherit ">Day</th>
+                                    <th className="border border-inherit ">Operating hours</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {biz.business.hours.map((hour,i) =>
+                                    hour.open.map((time,i) => {
+                                        return(
+                                            <tr key={i}>
+                                                <td className="border border-inherit text-center">
+                                                    {days[i]}
+                                                </td>
+                                                <td className="border border-inherit text-center">
+                                                    {convert24To12(time.start)} - {convert24To12(time.end)}
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <div className="container mx-auto flex flex-col">
-                </div>
-            </div>
             }
         </>
     )
--- src/utils.tsx
+++ src/utils.tsx
@@ -3,11 +3,17 @@ import customParseFormat from "dayjs/plugin/customParseFormat";
 
 const days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
 
-function getTime(time: number) {
+function getTime(time: any) {
     return dayjs(time).format("hh:mm A");
+}
+
+function convert24To12(time: any) {
+    dayjs.extend(customParseFormat);
+    return dayjs(time,'HHmm').format('hh:mm A');
 }
 
 export {
     days,
-    getTime
+    getTime,
+    convert24To12
 }
